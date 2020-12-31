e939b5bf93ff929bf325f19f15722385115873b0
Takuya Shizukuishi <sititou70@gmail.com>
2020-12-31 13:38:43 +0900

    feat: watch mode

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+# option
+usage_exit() {
+  echo "Usage: $0 [-w(watch mode)]"
+  exit 1
+}
+
+WATCH_OPTION=""
+while getopts wh OPT; do
+  case $OPT in
+  w)
+    WATCH_OPTION="-pvc -view=none"
+    ;;
+  h)
+    usage_exit
+    ;;
+  \?)
+    usage_exit
+    ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+# build tex
 FILE_NAME=$(basename $1 .tex)
 
 docker run \
   --rm \
   -v $(pwd)/:/latex/ \
-  sititou70/latex:v2.0.0 latexmk -pdfdvi $FILE_NAME
+  sititou70/latex:v2.0.0 latexmk -pdfdvi $WATCH_OPTION $FILE_NAME
 
+# remove unnecessary files
 REMOVE_EXTS=(
   aux
   dvi
